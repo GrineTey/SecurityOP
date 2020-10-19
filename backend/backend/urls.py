d1c51bc2c91442c2a129e7f15b6b59e59bd3b91b
Endriw Villa <Endriw_villa@icloud.com>
2020-10-19 10:03:32 -0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+"""Tentar adicionar no urlpatters a rota do tema"""
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
